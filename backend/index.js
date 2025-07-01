b14c99f64a969f1f0da68e735cccb8baddb62e93
bob-th <67490142+bob-th@users.noreply.github.com>
2025-07-01 14:40:49 -0400

    barebones backend

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`web backend listening on port ${port}`);
+});
