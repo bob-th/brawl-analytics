b1ded8c0ab6be912b42616aa2940ab98b7b21b10
charlesgchen <charlesg.chen@mail.utoronto.ca>
2025-07-05 00:41:28 -0400

    feature: added styling to main page, added login and about pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -12,36 +12,50 @@ function App() {
       enter.preventDefault()
       setShow(true)
     }
-
+    //call api
   }
   return (
     <>
-      
       <title>Brawl Analytics</title>
-      <header className=''>
-        <div className="border border-red-500 w-screen flex justify-between items-center">
-          <div className="flex">
-          <img 
-            src="/img/bslogo.png"
-            className="h-48 w-48 object-contain border border-red-50"
-          />
+      <header className='bg-radial-[at_95%_5%] from-nice_purple/50 from-5% to-gray-900'>
+        <div className="flex justify-between items-center">
+          <div className='flex justify-between items-center'>
+            <div className="flex p-4">
+              <img 
+                src="/img/bslogo.png"
+                className="object-contain "
+              />
+            </div>
+            <h1 className='font-hello text-3xl font-bold text-purple-50'>
+              Brawl Analytics
+            </h1>
           </div>
-          <h1 className='font-Oswald text-center'>
-            Brawl Analytics
-          </h1>
-          <div className="w-48"></div>
+          <div className="flex mr-4 space-x-2">
+            <button type="button" className="px-12 hover:underline">
+              Login
+            </button>
+            <button type="button" className="px-12 hover:underline">
+              About
+            </button>
+          </div>
+
         </div>
       </header>
-      <div>
 
-        <label>Enter your name: </label>
-        <input type="text" 
+      <div className='flex flex-col min-h-[75vh] justify-center items-center'>
+        
+        <h1 className='p-4 text-white font-hello font-medium'>Enter your ID.</h1>
+        <br />
+        <input 
+        className='w-64 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+        type="text" 
         name="playerid" 
-        placeholder='PlayerID'
+        placeholder='#'
         onChange={onChangeHandler}
         onKeyDown={onEnter}
         value={name}></input>
       </div>
+  
       <PlayerInfo playerID={name} show={show}></PlayerInfo>
 
       
